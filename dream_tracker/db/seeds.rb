8093e1d1edee6d3ba74b292825503a7224d0ad83
Ben Winter <ben.g.winter@gmail.com>
2013-10-15 16:20:07 -0400

    seeded db

--- dream_tracker/db/seeds.rb
+++ dream_tracker/db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Dream.create(name: "The Joyous Dream", description: "Joyous", scary:false, fun:true)
+Dream.create(name: "The Angry Dream", description: "Angry", scary:true, fun:false)
+Dream.create(name: "The Evil Dream", description: "Evil", scary:true, fun:false)
