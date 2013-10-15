036b8c3e6b83c8994db53d61b2c8ec3ec9d6e728
Ben Winter <ben.g.winter@gmail.com>
2013-10-15 15:57:15 -0400

    set up rails file

---
+++ dream_tracker/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DreamTracker::Application.config.secret_key_base = 'c9cc23c94dfa5e1944fca7e8e563a26b5b6383594054ae3fa98b448ddb40c528b1c43dfdd2db5e9631766148f827871b6a978601c3992ebcd87274d6c7952bc1'
