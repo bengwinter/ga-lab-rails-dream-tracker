8e6ee7a51e39aa96c99f626d82b1380a18d105ac
Ben Winter <ben.g.winter@gmail.com>
2013-10-15 16:15:14 -0400

    added dream.rb and dreams controller

---
+++ dream_tracker/app/controllers/dreams_controller.rb
@@ -0,0 +1,49 @@
+class DreamsController < ApplicationController
+  before_action :set_dream, only: [:name, :description, :scary, :fun]
+
+  def show
+  end
+
+  def index
+    @dreams = Dream.all
+  end
+
+  def new 
+    @dream = Dream.new
+  end
+
+  def create
+    @dream = Dream.new(dream_params)
+    if @dream.save
+      redirect_to @dream
+    else 
+      render action: 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @dream.update(dream_params)
+      redirect_to @dream
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @dream.destroy
+    redirect_to dreams_url
+  end
+
+  private
+  def dream_params
+      params.require(:dream).permit(:name, :description, :scary, :fun)
+  end
+
+  def set_dream
+    @dream = Dream.find(params[:id])
+  end
+
+end
